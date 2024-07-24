49f01bf544ab3db571a73595f119e49d16eabea8
空巷一人 <1530688385@qq.com>
2024-07-24 20:22:00 +0800

    添加前端样式示例

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,23 @@ hexo.extend.filter.register('after_post_render', async function (data) {
   if (config.default_enable) data.ai_enable = data.ai_enable === false ? false : true;
 
   if (!data.ai_enable || data.aiexcerpt) return data;
-  
 
   const content = strip(data.content, config.ignoreEl);
   log.info(`摘要 ${data.title} 完成`);
   const path = hexo.source_dir + data.source;
   const frontMatter = fm.parse(await fs.readFile(path));
 
-  frontMatter.aiexcerpt = data.aiexcerpt = await ai(config.api, config.model, content, config.prompt);
+  frontMatter.aiexcerpt = data.aiexcerpt = await ai(
+    config.api,
+    config.model,
+    content,
+    config.prompt
+  );
   await fs.writeFile(path, `---\n${fm.stringify(frontMatter)}`);
   return data;
+});
+
+hexo.extend.filter.register('after_post_render', function (data) {
+  if (data.ai_enable && data.aiexcerpt)
+    return (data.content = data.content + `<script>var aiexcerpt="${data.aiexcerpt}"</script>`);
 });
